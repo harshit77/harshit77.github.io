6549f5f257b96d0bb3714518a335fd16b7b497e6
Harshit Sahu <hsahu77@gmail.com>
2024-09-18 10:52:52 +0530

    Updated resume and link (#2)

--- app/_components/ui/magic-button.tsx
+++ app/_components/ui/magic-button.tsx
@@ -1,27 +1,44 @@
-
+import type { ElementType } from "react";
+import Link from "next/link"
 
 type MagicButtonProps ={
     title:string,
+    href?:string,
     iconPosition?:"left" | "right",
     icon:React.ReactNode
+    download?:boolean
     handleClick?:(event:React.MouseEvent<HTMLButtonElement>)=>void
 }
 
 const MagicButton = ({
     title,
+    href,
     iconPosition="right",
     icon,
     handleClick
 }:MagicButtonProps) => {
+     let RenderedComponent:ElementType, elementSpecificProps;
+   if(href) {
+    RenderedComponent= Link;
+    elementSpecificProps={
+      href,
+    }
+  }
+  else {
+  RenderedComponent="button";
+    elementSpecificProps={
+      onClick:handleClick
+    }
+  }
   return (
-     <button className="relative inline-flex h-12 overflow-hidden rounded-md p-[1px]" onClick={handleClick}>
+     <RenderedComponent className="relative inline-flex h-12 overflow-hidden rounded-md p-[1px]" {...elementSpecificProps}>
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
         <span className="inline-flex h-full w-full cursor-pointer items-center justify-center  bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl rounded-md gap-4">
             {iconPosition === "left" && icon}
             {title}
             {iconPosition === "right" && icon}
         </span>
-      </button>
+      </RenderedComponent>
   )
 }
 
