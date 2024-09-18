da7597c396c5757702b2e6b9713c18460a90b38e
Harshit Sahu <hsahu77@gmail.com>
2024-09-18 10:57:59 +0530

    Updated Lamp container parameter

--- app/_components/present/index.tsx
+++ app/_components/present/index.tsx
@@ -30,7 +30,7 @@ export default function Present() {
     <LampContainer>
       <motion.h1
         initial={{ opacity: 0.5, y: 300 }}
-        whileInView={{ opacity: 1, y: 200 }}
+        whileInView={{ opacity: 1, y: 180 }}
         transition={{
           delay: 0.3,
           duration: 0.8,
